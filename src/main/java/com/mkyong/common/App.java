f522c701df45fe007d281728592fe4497cda0b0b
lnvithanage <lnvithanage@virtusa.com>
2019-09-28 18:03:31 +0530

    Add new violation

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -3,6 +3,9 @@ package com.mkyong.common;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.stream.*;
 
 /**
@@ -17,5 +20,13 @@ public class App {
 
 
         System.out.println("abc");
+        System.out.println("abc");
+        System.out.println("abc");
+
+        try {
+            OutputStream os = new FileOutputStream("/file");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
