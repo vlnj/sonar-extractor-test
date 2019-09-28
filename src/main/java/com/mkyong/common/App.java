e73d54a7f3e11d50ba3ac0c04aca4f5b8a40371d
lnvithanage <lnvithanage@virtusa.com>
2019-09-28 11:48:11 +0530

    Add project

---
+++ src/main/java/com/mkyong/common/App.java
@@ -0,0 +1,21 @@
+package com.mkyong.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.stream.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("abcd");
+        System.out.println();
+
+
+        System.out.println("abc");
+    }
+}
---
+++ src/main/java/com/mkyong/common/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.mkyong.common;
+
+/**
+ * Hello world!
+ */
+public class HelloWorld {
+    private static String name = "Lasitha";
+
+    public void printHello() {
+        System.out.println("Hello ! " + name);
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private int getAge(){
+        return 0;
+    }
+}
