7aa233d0187fb4fbba5c171d63f2083351309256
lnvithanage <lnvithanage@virtusa.com>
2019-09-28 18:08:59 +0530

    Add new file

---
+++ src/main/java/com/mkyong/common/Project.java
@@ -0,0 +1,34 @@
+package com.mkyong.common;
+
+public class Project {
+
+    public int id;
+
+    public String name;
+
+    public String extId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtId() {
+        return extId;
+    }
+
+    public void setExtId(String extId) {
+        this.extId = extId;
+    }
+}
